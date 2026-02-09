c1836eb85dfc0dad225f5b387b95937337384acf
Neshad D. Pathirana <112592828+neshaddp@users.noreply.github.com>
2026-02-09 17:37:17 -0500

    Update content.js

--- src/content.js
+++ src/content.js
@@ -29,10 +29,10 @@ export const nav = [
 ];
 
 // Affiliation logos shown on the Home section
-export const affiliationLogos = [ 
+export const affiliationLogos = [
   { name: "Michigan State University", src: "/images/logos/MSU.jpg", href: "https://msu.edu", variant: "light" },
   { name: "Facility for Rare Isotope Beams (FRIB)", src: "/images/logos/FRIB.jpg", href: "https://frib.msu.edu", variant: "dark" },
-  { name: "NuGrid", src: "/images/logos/NuGrid.png"", href: "https://nugridstars.org", variant: "dark" },
+  { name: "NuGrid", src: "/images/logos/NuGrid.png", href: "https://nugridstars.org", variant: "dark" },
   { name: "IReNA", src: "/images/logos/IReNA.jpg", href: "https://www.irenaweb.org", variant: "light" },
   { name: "CeNAM", src: "/images/logos/CeNAM.png", href: "https://cenam.nd.edu", variant: "light" },
   { name: "JINA-CEE", src: "/images/logos/JINA.png", href: "https://jinaweb.org", variant: "dark" },
