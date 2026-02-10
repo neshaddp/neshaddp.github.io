c79004e5adb631de11c33fba73b902e7181d9eb2
Neshad D. Pathirana <112592828+neshaddp@users.noreply.github.com>
2026-02-09 23:47:34 -0500

    Update content.js

--- src/content.js
+++ src/content.js
@@ -529,7 +529,7 @@ export const projects = [
     title: "Indirect Constraints on Neutron-Capture Reaction Rates",
     timeframe: "Published + Ongoing",
     role: "Graduate Research Assistant (FRIB/MSU) • Visiting Scholar (University of Oslo)",
-    image: "/images/nuclides_chart1.png",
+    image: "/images/nuclides_chart2.png",
     tags: ["Hauser–Feshbach", "TALYS", "NLD", "γSF"],
     summary:
       "Constraining astrophysical (n,γ) rates by incorporating experimentally extracted statistical inputs into Hauser–Feshbach calculations.",
